d31cc03c104e560e57f4eb8c9314ed40980aecc1
sajaalmuraqtn <sajaalmuraqtn@gmail.com>
2025-11-17 20:16:44 +0200

    comment

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:quiz_app_design/screens/home.dart';
 void main() {
   runApp( MaterialApp(
    debugShowCheckedModeBanner: false,
-    home: HomeScreen()) ,
+    home: HomeScreen()) ,//home page
   ) ;
 }
 
